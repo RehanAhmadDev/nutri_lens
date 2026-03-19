78fe972633b071745d68685bfefc969bd22f7277
RehanAhmadDev <rehanappdev@gmail.com>
2026-03-19 20:46:42 +0500

    Secured project: Added api_constants to .gitignore and removed from cache

--- lib/utils/api_constants.dart
+++
@@ -1,8 +0,0 @@
-class ApiConstants {
-  // Aap ki Gemini Key
-  static const String geminiApiKey = '';
-
-  // 🚀 Aap ki Supabase Keys
-  static const String supabaseUrl = '';
-  static const String supabaseAnonKey = '';
-}
